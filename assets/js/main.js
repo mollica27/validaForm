1048be39dc4969eaaa03d53f008f8ebe65412ab1
Natan Mollica <natan.mollica@gmail.com>
2023-04-10 22:30:33 -0300

    campoErro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,36 @@
 class ValidaFormulario {
     constructor() {
+        this.formulario = document.querySelector('.formulario');
         this.eventos();
     }
 
     eventos() {
-        
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const camposValidos = this.camposSaoValidos();
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+
+        for(let campo of this.formulario.querySelectorAll('.validar')) {
+            if(!campo.value) {
+                this,criaErro(campo, 'Campo nao pode estar em branco.')
+            }
+        }
+    }
+
+    criaErro(campo, msg) {
+        const div = document.createElement('div');
+        div,innerHTML = msg;
+        div,classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
     }
 }
+
+const valida = new ValidaFormulario();
